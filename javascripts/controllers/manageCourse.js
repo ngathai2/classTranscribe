51c5a85edd670689d1c6974f201091ea1bdac574
Nga Thai <ngamythai@gmail.com>
2017-04-16 14:42:50 -0500

    filter search

---
+++ javascripts/controllers/manageCourse.js
@@ -0,0 +1,52 @@
+
+/* source: http://stackoverflow.com/questions/11406605/how-to-make-a-link-act-as-a-file-input */
+/* when "upload-link" is clicked, trigger the event for the upload button*/
+$(function(){
+	$("#upload-link").on('click', function(event){
+		event.preventDefault();
+		$("#upload-file:hidden").trigger('click');
+        $("#upload-file").data('clicked', true);
+	})
+});
+
+/* after the file has been uploaded, display it so the user knows which file was chosen*/
+function getFilename(file_upload) {
+    var file = file_upload.files[0];  
+    var filename = file.name;
+    $("#filename i").html(filename);
+}
+
+/* parse the text box of instructors */
+$(function() {
+    $("#instructor-button").on('click', function(event) {
+        var instructors = $("#instructor-box").val();
+        $("#i-test").html(instructors);
+    })
+});
+    
+/* parse the text box of students or the uploaded file */
+$(function() {
+    $("#student-button").on('click', function(event) {
+        var students = $("#student-box").val();
+        $("#s-test").html(students);
+        if($("#upload-file").data('clicked')) {
+
+        }
+        else {
+
+        }
+    })
+});
+
+/* filter the search box */
+function filter(event) {
+    var query = $(".search").val().toLowerCase();
+    query = query.trim().replace(/[.,!"?()]/g,"").replace(/-/g, " ").split(/\s+/);
+    client.smembers("ClassTranscribe::Classes", function(err, result) {
+        if(result) {
+            result.forEach(function(member) {
+                $("search-results").append("<p>"+ member +"</p>")
+            });
+        }
+    });
+}
